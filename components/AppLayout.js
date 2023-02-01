02296a1b8ea524ce71a02bef7a3d0956f995eb07
JHKIMS <wjdgns5131@gmail.com>
2023-02-01 15:44:19 +0900

    Twitter: 더미데이터로 로그인 처리하기

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -2,11 +2,15 @@ import PropTypes from "prop-types";
 import Link from "next/link";
 import { useState,useCallback } from "react";
 import { Menu, Input, Row, Col } from "antd";
-
+import styled from 'styled-components';
 import UserProfile from "./UserProfile";
 import LoginForm from "./LoginForm";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`
 const AppLayout = ({ children }) => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -17,7 +21,7 @@ const AppLayout = ({ children }) => {
           <Link href="/profile">프로필</Link>
         </Menu.Item>
         <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+          <SearchInput enterButton />
         </Menu.Item>
         <Menu.Item>
           <Link href="/signup">회원가입</Link>
@@ -25,7 +29,7 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn}/> : <LoginForm setIsLoggedIn={setIsLoggedIn}/>}
         </Col>
         <Col xs={24} md={12}>
           {children}
--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,4 +1,25 @@
-const UserProfile = () => {
+import { Card, Avatar, Button } from "antd";
+import { useCallback } from "react";
 
-}
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogOut = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+  return (
+    <>
+      <Card
+        actions={[
+            <div key="twit">우아!<br />0</div>,
+            <div key="following">팔로잉<br />0</div>,
+            <div key="follower">팔로워<br />0</div>,
+        ]}
+      >
+
+
+        <Card.Meta avatar={<Avatar>Sample</Avatar>} title="Hoon" />
+        <Button onClick={onLogOut}>로그아웃</Button>
+      </Card>
+    </>
+  );
+};
 export default UserProfile;
