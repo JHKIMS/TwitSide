61e6be0ed95b9c5ae39f7ac9ddc5fab918dad05f
JHKIMS <wjdgns5131@gmail.com>
2023-02-01 09:54:54 +0900

    Twitter: 로그인폼 만들기

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,39 +1,39 @@
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { useState,useCallback } from "react";
 import { Menu, Input, Row, Col } from "antd";
 
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
         <Menu.Item>
-          <Link href="/">
-            노드버드
-          </Link>
+          <Link href="/">노드버드</Link>
         </Menu.Item>
         <Menu.Item>
-          <Link href="/profile">
-            프로필
-          </Link>
+          <Link href="/profile">프로필</Link>
         </Menu.Item>
         <Menu.Item>
-            <Input.Search enterButton style={{verticalAlign: 'middle'}} />
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
         </Menu.Item>
         <Menu.Item>
-          <Link href="/signup">
-            회원가입
-          </Link>
+          <Link href="/signup">회원가입</Link>
         </Menu.Item>
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
         </Col>
         <Col xs={24} md={6}>
-          <a href="https://velog.io/" target="_blank" rel="noreferrer noopener">Velog</a>
+          <a href="https://velog.io/" target="_blank" rel="noreferrer noopener">
+            Velog
+          </a>
         </Col>
       </Row>
     </div>
