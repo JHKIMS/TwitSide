1b787db03a3d66b22d2772e09f14c69dd7ca7bcd
JHKIMS <wjdgns5131@gmail.com>
2023-02-02 20:54:10 +0900

    Twitter: Reducer분리하기

--- reducers/index.js
+++ reducers/index.js
@@ -1,55 +1,21 @@
 import { HYDRATE } from "next-redux-wrapper";
-
-const originState = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    signUpData: {},
-    loginData: {},
-  },
-  post: {
-    mainPosts: [],
-  },
-};
-//Action Creator
-export const loginAction = (data) => {
-  return {
-    type: "LOG_IN",
-    data,
-  };
-};
-export const logoutAction = () => {
-  return {
-    type: "LOG_OUT",
-  };
-};
+import user from "./user";
+import post from "./post";
+import { combineReducers } from "redux";
 
 //Reducer : (이전상태, 액션)으로 다음상태를 만들어내는 역할을 한다.
-const rootReducer = (state = originState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      return { ...state, ...action.payload };
-    case "LOG_IN":
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: true,
-          user: action.data,
-        },
-      };
-    case "LOG_OUT":
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: false,
-          user: null,
-        },
-      };
-    default:
-      return state;
-  }
-};
+// combineReducers : 단어 그대로 Reducer를 합쳐준다.
+const rootReducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        return { ...state, ...action.payload };
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 
 export default rootReducer;
