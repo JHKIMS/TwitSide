01f2fc871596debbad1972bc5cc91480b9dd97eb
JHKIMS <wjdgns5131@gmail.com>
2023-02-02 13:03:59 +0900

    Twitter: 프로필 페이지 만들기

--- pages/profile.js
+++ pages/profile.js
@@ -1,14 +1,22 @@
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
 import React from 'react';
+import NickNameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const profile = () => {
+  const followerList = [{nickname: 'TwitDummy'},{nickname: 'TwitDummy2'},{nickname: 'TwitDummy3'}]
+  const followingList = [{nickname: 'TwitDum'},{nickname: 'TwitDum2'},{nickname: 'TwitDum3'}]
   return (
     <>
       <Head>
         <div>내 Profile | TwitHoon</div>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NickNameEditForm></NickNameEditForm>  
+        <FollowList header="팔로잉 목록" data={followerList}/>
+        <FollowList header="팔로워 목록" data={followingList}/>
+      </AppLayout>;
     </>
   );
 };
