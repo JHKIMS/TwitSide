69c68500f0679aa58efdf4b6dd475112c014ac7d
JHKIMS <wjdgns5131@gmail.com>
2023-02-05 11:34:10 +0900

    Twitter: Redux-Saga 설치

--- store/configStore.js
+++ store/configStore.js
@@ -2,14 +2,18 @@ import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, configureStore } from "@reduxjs/toolkit";
 import reducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "@redux-saga/core";
+import rootSaga from '../sagas';
 
 const configStore = () => {// state와 reducer를 포함하는게 Store이다.
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
-  const store = configureStore({reducer, enhancer});
+  const store = configureStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 configStore();
