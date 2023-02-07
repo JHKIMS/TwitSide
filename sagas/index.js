bcebaec968cb49ab1ae5e5c9d7cea46b3c8ff968
JHKIMS <wjdgns5131@gmail.com>
2023-02-07 09:42:59 +0900

    Twitter: take,takeEvery사용법

--- sagas/index.js
+++ sagas/index.js
@@ -1,66 +1,66 @@
-import { all, fork, call, take, put } from "redux-saga/effects";
+import { all, fork, call, take, put, takeEvery, delay } from "redux-saga/effects";
 import axios from "axios";
 
 function logInAPI(data) {
-  return axios.post("/api/login",data);
+  return axios.post("/api/login", data);
 }
 function* logIn(action) {
   try {
-    const result = yield call(logInAPI, action.data); // yield는 await과 비슷하다.
+    yield delay(1000);
+    // const result = yield call(logInAPI, action.data);  // yield는 await과 비슷하다.
     yield put({
       type: "LOG-IN_SUCCESS",
-      data: result.data,
     });
   } catch (err) {
     yield put({
-        type: 'LOG_IN_FAILURE',
-        data: err.response.data,
-    })
+      type: "LOG_IN_FAILURE",
+      data: err.response.data,
+    });
   }
 }
-function logOutAPI(){
+function logOutAPI() {
   return axios.post("/api/logout");
 }
 function* logOut() {
   try {
-    const result = yield call(logOutAPI); // yield는 await과 비슷하다.
+    yield delay(1000);
+    // const result = yield call(logOutAPI); 
     yield put({
       type: "LOG_OUT_SUCCESS",
-      data: result.data,
     });
   } catch (err) {
     yield put({
-        type: 'LOG_OUT_FAILURE',
-        data: err.response.data,
-    })
+      type: "LOG_OUT_FAILURE",
+      data: err.response.data,
+    });
   }
 }
 
-function addPostAPI(data){
-  return axios.post("/api/post",data);
+function addPostAPI(data) {
+  return axios.post("/api/post", data);
 }
 function* addPost(action) {
   try {
-    const result = yield call(addPostAPI,action.data); // yield는 await과 비슷하다.
+    yield delay(1000);
+    // const result = yield call(addPostAPI, action.data);
     yield put({
       type: "ADD_POST_SUCCESS",
-      data: result.data,
     });
   } catch (err) {
     yield put({
-        type: 'ADD_POST_FAILURE',
-        data: err.response.data,
-    })
+      type: "ADD_POST_FAILURE",
+      data: err.response.data,
+    });
   }
 }
 function* watchLogIn() {
-  yield take("LOG_IN_REQUEST", logIn);
+    yield takeEvery("LOG_IN_REQUEST", logIn);
 }
 function* watchLogOut() {
-  yield take("LOG_OUT_REQUEST",logOut);
+    yield takeEvery("LOG_OUT_REQUEST", logOut);
 }
 function* watchAddPost() {
-  yield take("ADD_POST_REQUEST",addPost);
+    yield takeEvery("ADD_POST_REQUEST", addPost);
 }
 
 export default function* rootSaga() {
